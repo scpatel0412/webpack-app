0ede50b11425a60e428119d735e47919007710a9
sagar.patel <sagar.patel@iflair.com>
2024-07-26 15:54:26 +0530

    added middel ware in browser sync

--- webpack.config.js
+++ webpack.config.js
@@ -59,8 +59,8 @@ module.exports = {
         port: 9000, // Port to run the development server on
         historyApiFallback: {
             rewrites: [
-                { from: /^\/santorini$/, to: `${__dirname}/src/santorini/santorini.html` },
-                { from: /^\/mykonos$/, to: './src/mykonos/mykonos.html' }
+                { from: /^\/santorini$/, to: `/src/santorini/santorini.html` },
+                { from: /^\/mykonos$/, to: `/src/mykonos/mykonos.html` }
             ]
         }
     },
@@ -72,7 +72,27 @@ module.exports = {
             port: process.env.PORT,
             server: { baseDir: ['dist'] },
             files: ['./dist/*'],
-            notify: false
+            notify: false,
+            middleware: [
+                function (req, res, next) {
+                    // Handle redirects
+                    if (req.url.startsWith('/src/')) {
+                        const entry = req.url.split('/')[2]; // Extract the entry name
+                        if (Object.keys(entries).includes(entry)) {
+                            res.writeHead(301, { Location: `/${entry}` });
+                            res.end();
+                        } else {
+                            next(); // Not an entry point, continue
+                        }
+                    } else if (Object.keys(entries).includes(req.url.substring(1))) {
+                        // Handle rewriting of /entry to /src/entry/entry.html
+                        req.url = `/src/${req.url.substring(1)}/${req.url.substring(1)}.html`;
+                        next();
+                    } else {
+                        next(); // No special handling, continue
+                    }
+                }
+            ]
         }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
